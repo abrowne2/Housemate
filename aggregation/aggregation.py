1f00046c65ced345b9fed48924a574fac85ef1a9
Adam Browne <adamo.browne@gmail.com>
2016-10-09 00:33:11 -0400

    preferentialSearch & model overhaul

--- aggregation/aggregation.py
+++ aggregation/aggregation.py
@@ -21,7 +21,7 @@ Pertinent information is stored accordingly.
 
 '''
 
-
+#used as ACParser
 def autoComplete(input):
     # rent.com uses "ONESEARCH", and you can make calls to their query endpoint for JSON responses, nifty:
     univJSON = requests.get('http://onesearch.svc.primedia.com/autocomplete?q=' + input + '&application=rent')
@@ -32,49 +32,39 @@ def autoComplete(input):
 
     return universities
 
-def userOption(input):
-    pass
+def performChoice(hbool, abool, seopath, budget, beds):
+    listIds = '' #list of listing ids used for traversal
 
-def rentCom(college):
-    universities = autoComplete(college)
+    if hbool is True and abool is True: #User does not care. Fetch both houses/apartments.
+        haws = requests.get('http://rent.com' + seopath + 'condos_houses_townhouses' + (('_max-price-' + budget) + '_' + beds + '-bedroom'))
+        cribz = requests.get('http://rent.com' + seopath + 'apartments_condos_townhouses' + (('_max-price-' + budget) + '_' + beds + '-bedroom'))
+        listIds = ','.join(BeautifulSoup(haws.text, "html.parser").find(attrs={"name": "listing_ids"})['content'].split(';')) +\
+            ','.join(BeautifulSoup(cribz.text, "html.parser").find(attrs={"name": "listing_ids"})['content'].split(';'))
+    elif hbool and abool is False: #user wants to find a house.
+        haws = requests.get('http://rent.com' + seopath + 'condos_houses_townhouses' + (('_max-price-' + budget) + '_' + beds + '-bedroom'))
+        listIds = ','.join(BeautifulSoup(haws.text, "html.parser").find(attrs={"name": "listing_ids"})['content'].split(';'))
+    else: #user wants an apartment
+        cribz = requests.get('http://rent.com' + seopath + 'apartments_condos_townhouses' + (('_max-price-' + budget) + '_' + beds + '-bedroom'))
+        listIds = ','.join(BeautifulSoup(cribz.text, "html.parser").find(attrs={"name": "listing_ids"})['content'].split(';'))
 
-    univ = int(input()) - 1 #mark the user's college here.
-    print("What are you looking for? House, Apartment, or both?\n")
-    seek = input()
+    return listIds
 
-    #we have to effectively change the print calls into requests made to the FB platform
+#given a url_path, fetch more information about the specified property
+def rentPropertyTraversal(listIds, univLoc, usrDist):
+    #make a call to the rent.com listings.json endpoint, and get all the data required for interpretation.
+    propJSON = json.loads(requests.get('http://rent.com/account/myrent/listings.json?ids=' + listIds).text)
 
-    if seek == 'House':
-        choice = requests.get('http://rent.com' + universities[univ][1] + 'condos_houses_townhouses/')
-        listIds = ','.join(BeautifulSoup(choice.text, "html.parser").find(attrs={"name": "listing_ids"})['content'].split(';'))
-    elif seek == 'Apartment':
-        choice = requests.get('http://rent.com' + universities[univ][1] + 'apartments_condos_townhouses/')
-        listIds = ','.join(BeautifulSoup(choice.text, "html.parser").find(attrs={"name": "listing_ids"})['content'].split(';'))
-    elif seek == 'Both':
-        choice = requests.get('http://rent.com' + universities[univ][1])
-
-
-    #retrieve the list of listing_ids, which we can use to obtain a JSON object of all the properties.
-
-
-    #below, you don't even have to be authenticated to provide the list of listing_ids...
-    propJSON = requests.get('http://rent.com/account/myrent/listings.json?ids=' + listIds)
-    propJSON = json.loads(propJSON.text)
     #list comprehension technique here: this is actually a dictionary inside a list. refer to the values by key,
     properties = [{'url_path': x['url_path'], 'name': x['name'], 'address': x['address'], 'city': x['city'],
                     'state': x['state'], 'price_range': x['price_range'], 'bedroom_range': x['bedroom_range'],
                     'bathroom_range': x['bathroom_range'], 'image_url': x['image_url']} for x in propJSON['listings']]
-
-    rentPropertyTraversal(properties, universities[univ][2].split(','))
-
-
-#given a url_path, fetch more information about the specified property
-def rentPropertyTraversal(properties, univLoc):
     for x in properties:
         propInfo = BeautifulSoup(requests.get('http://rent.com' + x['url_path']).text, "html.parser")
         geocode = propInfo.findAll(True, {"property": ['place:location:longitude', 'place:location:latitude']})
-        x['crime'] = crime.fetch(geocode[0]['content'], geocode[1]['content'])
         x['dist_campus'] = distance.haversine(float(univLoc[1]), float(univLoc[0]), float(geocode[0]['content']),
-                                     float(geocode[1]['content']))
-        print(x['name'] + ' crime: ' + str(x['crime']) + ' distance: ' + str(x['dist_campus']))
+                                              float(geocode[1]['content']))
+        x['withinRange'] = True if x['dist_campus'] < usrDist else False
+        if x['withinRange']:
+            x['crime'] = crime.fetch(geocode[0]['content'], geocode[1]['content'])
+    return properties
 
