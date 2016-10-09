74fc1f9143c693eb9c7b43a24b24004f50e66f4f
Noah <noahfren@umich.edu>
2016-10-08 20:27:37 -0400

    Debugging main

--- messenger/main.py
+++ messenger/main.py
@@ -26,10 +26,12 @@ main = Blueprint('main', __name__)
 
 @main.route('/', methods=['GET', 'POST'])
 def main_route():
+	print("Request recieved!")
 	if request.method == 'POST':
+		print("And it was a POST request!\n")
 		try:
-			payload = request.getData()
-			for message, sender in message_events(payload):
+			payload = request.data
+			for message, sender in messaging_events(payload):
 				# if a convo with this sender exists, run the appropriate protocol
 				found = convos.find_one({"id": sender})
 				temp = Conversation();
@@ -50,9 +52,10 @@ def main_route():
 			return "okay"
 			
 		except Exception as e:
-			print("Something went wrong")
+			print(e)
 
 	elif request.method == 'GET':
+		print("And it was a GET request!\n")
 		if request.args.get('hub.verify_token') == config.env['verify_token']:
 			return request.args.get('hub.challenge')
 		return "Wrong Verify Token"
@@ -61,7 +64,7 @@ def main_route():
 
 #function to generate a list of [id, messageText] from payload
 def messaging_events(payload):
-	data = json.loads(request.data)
+	data = json.loads(payload)
 	messaging_events = data["entry"][0]["messaging"]
 	
 	for event in messaging_events:
@@ -74,7 +77,7 @@ def send_message (recipient, text):
 	
 	global token
 	r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, data=json.dumps({
-	"reciepient": {"id": recipient},
+	"recipient": {"id": recipient},
 	"message":{"text":
 	           text.decode('unicode_escape')}
 	           }),
