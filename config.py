90761c068257038dbb0deb7d93452812d5c86120
Noah <noahfren@umich.edu>
2016-10-09 10:32:58 -0400

    Cleaned up repo

--- config.py
+++
@@ -1,6 +0,0 @@
-env = {
-	'host' : '0.0.0.0',
-	'port' : 3000,
-	'access_token' : 'EAACxq6YZA0kABAHqhZBGm363gjGMgZCzEmg8aZBeZCZC9ap298nZBn6Nq7WVTPBmTDHERmPG6qw4uL3VcEdtfZAqcK1DpSZCEKns9L5vsZBjkMHPOwZAa4eOAu6Af8AiHWqMa58qdxd8apTX23Ouh8rJ47T7gOK8hRZBnGGIzGZBd973SBAZDZD',
-	'verify_token' : 'gvbafcily6htwreoaiugqf0o'
-}
