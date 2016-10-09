c49aea8c7faa87dba248a73d95b0f6830c97fe00
Noah <noahfren@umich.edu>
2016-10-08 23:17:18 -0400

    Squashing all the bugs

--- messenger/main.py
+++ messenger/main.py
@@ -1,6 +1,7 @@
 from flask import *
 import sys
 sys.path.insert(0, '.')
+import config
 import json
 import traceback
 import random
@@ -11,52 +12,58 @@ from pymongo import MongoClient
 token = config.env['access_token']
 
 #add in the mongo server in the instance call
-client = MongoClient()
-convos = client.conversations
+#client = MongoClient()
+#convos = client.conversations
 
-responses = ["What university do you attend?\n", # Convo state 0
-				"Did you mean:\n",  # Convo state 1
-				"Are you looking for an appartment, house or both?\n", # Convo state 2
-				"How many bedrooms?\n", # Convo state 3
-				"What's your price range?\n", # Convo state 4
-                                "Are you concerned about crime in your neighborhood?" # Convo state 5
-                                ]
+convos = {}
+
+responses = ["Did you mean:\n",  # Convo state 0
+			"Are you looking for an appartment, house or both?\n", # Convo state 1
+			"How many bedrooms?\n", # Convo state 2
+			"What's your price range?\n" # Convo state 4
+			]
 			
 
 main = Blueprint('main', __name__)
 
 @main.route('/', methods=['GET', 'POST'])
 def main_route():
-	print("Request recieved!")
 	if request.method == 'POST':
-		print("And it was a POST request!\n")
 		try:
-			payload = request.data
+			payload = request.get_json()
 			for message, sender in messaging_events(payload):
 				# if a convo with this sender exists, run the appropriate protocol
-				found = convos.find_one({"id": sender})
-				temp = Conversation();
-				if  found != "":
+				#found = convos.find_one({"id": sender})
+				#temp = Conversation(sender);
+				#if  found != "":
 					
-					temp.prefs = found["prefs"]
-					temp.curState = found["curState"]
-					temp.id = found["id"]
-					temp.numBeds = found["numBeds"]
+					#temp.prefs = found["prefs"]
+					#temp.curState = found["curState"]
+					#temp.id = found["id"]
+					#temp.numBeds = found["numBeds"]
 					
-					parse_and_respond(temp, message)
-					convos.update_one({"id": sender}, temp)
+					#parse_and_respond(temp, message)
+					#convos.update_one({"id": sender}, temp)
+				#else:
+					#convos.insert_one(temp)
+					#message = "Initial question  as result of function here"
+
+				#Temporarily using a python dict to store data
+				if sender in convos:
+					parse_and_respond(convos[sender], message)
+
 				else:
-					convos.insert_one(temp)
-					message = "Initial question  as result of function here"
+					convos[sender] = Conversation(sender)
+					parse_and_respond(convos[sender], message)
 
 
 			return "okay"
 			
 		except Exception as e:
-			print(e)
+			print(type(e))
+			print(e.args)
 
 	elif request.method == 'GET':
-		print("And it was a GET request!\n")
 		if request.args.get('hub.verify_token') == config.env['verify_token']:
 			return request.args.get('hub.challenge')
 		return "Wrong Verify Token"
@@ -65,24 +72,21 @@ def main_route():
 
 #function to generate a list of [id, messageText] from payload
 def messaging_events(payload):
-	data = json.loads(payload)
+	data = payload
 	messaging_events = data["entry"][0]["messaging"]
 	
 	for event in messaging_events:
 		if "message" in event and "text" in event["message"]:
-			yield event ["sender"]["id"], "test"
+			yield event["message"]["text"], event["sender"]["id"]
 			
 			
 #send the message 'text' to 'recipient'			
 def send_message (recipient, text):
-	global token
 	r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, data=json.dumps({
 	"recipient": {"id": recipient},
-	"message":{"text":
-	           text.decode('unicode_escape')}
-	           }),
-			  headers={'content-type':'application/json'})
-	print("Post request sent")
+	"message":{"text": text}
+	}),
+	headers={'content-type':'application/json'})
 	if r.status_code != requests.codes.ok:
 		print(r.text)
 
@@ -90,28 +94,55 @@ def send_message (recipient, text):
 def parse_and_respond(convo, message):
 	convo_state = convo.curState
 	if convo_state == 0:
-		send_message(convo.id, responses[0])
+		try:
+			convo.ACParser(message)
+			send_message(convo.id, responses[0] + convo.acResultsToString())
+			convo.incrState()
+		except Exception as e:
+			send_message(convo.id, "I'm sorry, I didn't quite get that, can you rephrase?")
+			convo.curState = 0
 
 	elif convo_state == 1:
-		convo.ACParser(message)
-		send_message(convo.id, responses[1] + convo.acResultsToString)
+		try:
+			convo.acIndexParse(message)
+			send_message(convo.id, responses[1])
+			convo.incrState()
+		except Exception as e:
+			send_message(convo.id, "I'm sorry, I didn't quite get that, can you rephrase?")
+			convo.curState = 1
 
 	elif convo_state == 2:
-		convo.acIndexParse(message)
-		send_message(convo.id, responses[2])
+		try:
+			convo.optionPrs(message)
+			send_message(convo.id, responses[2])
+			convo.incrState()
+		except Exception as e:
+			send_message(convo.id, "I'm sorry, I didn't quite get that, can you rephrase?")
+			convo.curState = 2
 
 	elif convo_state == 3:
-		convo.bedBathPrs(message)
-		send_message(convo.id, responses[3])
+		try:
+			convo.bedBathPrs(message)
+			send_message(convo.id, responses[3])
+			convo.incrState()
+		except Exception as e:
+			send_message(convo.id, "I'm sorry, I didn't quite get that, can you rephrase?")
+			convo.curState = 3
 
 	elif convo_state == 4:
-		convo.pricePrs(message)
-		#return final results here
+		try:
+			convo.pricePrs(message)
+			#return final results here
+			send_message(convo.id, "Results go here:)")
+			convo.curState = 0
+			send_message(convo.id, "Where else would you like to look for housing?")
+		except Exception as e:
+			send_message(convo.id, "I'm sorry, I don't understand, can you rephrase?")
+			convo.curState = 4
 
 	else:
 		convo.curState = 0
 
-	convo.incrState()
 	return
 
 
