2b2c71ead00e1ecf28a727227d7ac44c13822f84
Adam Browne <adamo.browne@gmail.com>
2016-10-10 12:48:02 -0400

    Add timeout to requests to ensure they terminate.

--- aggregation/aggregation.py
+++ aggregation/aggregation.py
@@ -24,7 +24,7 @@ Pertinent information is stored accordingly.
 #used as ACParser
 def autoComplete(input):
     # rent.com uses "ONESEARCH", and you can make calls to their query endpoint for JSON responses, nifty:
-    univJSON = requests.get('http://onesearch.svc.primedia.com/autocomplete?q=' + input + '&application=rent')
+    univJSON = requests.get(('http://onesearch.svc.primedia.com/autocomplete?q=' + input + '&application=rent'), verify=False, timeout=10)
     univJSON = json.loads(univJSON.text)  # parse the JSON response.
 
     # use the seopath to retrieve the listing ids.
@@ -36,15 +36,15 @@ def performChoice(hbool, abool, seopath, budget, beds):
     listIds = '' #list of listing ids used for traversal
 
     if hbool is True and abool is True: #User does not care. Fetch both houses/apartments.
-        haws = requests.get('http://rent.com' + seopath + 'condos_houses_townhouses' + (('_max-price-' + budget) + '_' + beds + '-bedroom'))
-        cribz = requests.get('http://rent.com' + seopath + 'apartments_condos_townhouses' + (('_max-price-' + budget) + '_' + beds + '-bedroom'))
+        haws = requests.get(('http://rent.com' + seopath + 'condos_houses_townhouses' + (('_max-price-' + budget) + '_' + beds + '-bedroom')), verify=False, timeout=10)
+        cribz = requests.get(('http://rent.com' + seopath + 'apartments_condos_townhouses' + (('_max-price-' + budget) + '_' + beds + '-bedroom')), verify=False, timeout=10)
         listIds = ','.join(BeautifulSoup(haws.text, "html.parser").find(attrs={"name": "listing_ids"})['content'].split(';')) +\
             ','.join(BeautifulSoup(cribz.text, "html.parser").find(attrs={"name": "listing_ids"})['content'].split(';'))
     elif hbool and abool is False: #user wants to find a house.
-        haws = requests.get('http://rent.com' + seopath + 'condos_houses_townhouses' + (('_max-price-' + budget) + '_' + beds + '-bedroom'))
+        haws = requests.get(('http://rent.com' + seopath + 'condos_houses_townhouses' + (('_max-price-' + budget) + '_' + beds + '-bedroom')), verify=False, timeout=10)
         listIds = ','.join(BeautifulSoup(haws.text, "html.parser").find(attrs={"name": "listing_ids"})['content'].split(';'))
     else: #user wants an apartment
-        cribz = requests.get('http://rent.com' + seopath + 'apartments_condos_townhouses' + (('_max-price-' + budget) + '_' + beds + '-bedroom'))
+        cribz = requests.get(('http://rent.com' + seopath + 'apartments_condos_townhouses' + (('_max-price-' + budget) + '_' + beds + '-bedroom')), verify=False, timeout=10)
         listIds = ','.join(BeautifulSoup(cribz.text, "html.parser").find(attrs={"name": "listing_ids"})['content'].split(';'))
 
     return listIds
@@ -52,14 +52,14 @@ def performChoice(hbool, abool, seopath, budget, beds):
 #given a url_path, fetch more information about the specified property
 def rentPropertyTraversal(listIds, univLoc, usrDist):
     #make a call to the rent.com listings.json endpoint, and get all the data required for interpretation.
-    propJSON = json.loads(requests.get('http://rent.com/account/myrent/listings.json?ids=' + listIds).text)
+    propJSON = json.loads(requests.get(('http://rent.com/account/myrent/listings.json?ids=' + listIds), verify=False, timeout=10).text,)
 
     #list comprehension technique here: this is actually a dictionary inside a list. refer to the values by key,
     properties = [{'url_path': x['url_path'], 'name': x['name'], 'address': x['address'], 'city': x['city'],
                     'state': x['state'], 'price_range': x['price_range'], 'bedroom_range': x['bedroom_range'],
                     'bathroom_range': x['bathroom_range'], 'image_url': x['image_url']} for x in propJSON['listings']]
     for x in properties:
-        propInfo = BeautifulSoup(requests.get('http://rent.com' + x['url_path']).text, "html.parser")
+        propInfo = BeautifulSoup(requests.get(('http://rent.com' + x['url_path']), verify=False, timeout=10).text, "html.parser")
         geocode = propInfo.findAll(True, {"property": ['place:location:longitude', 'place:location:latitude']})
         x['dist_campus'] = distance.haversine(float(univLoc[1]), float(univLoc[0]), float(geocode[0]['content']),
                                               float(geocode[1]['content']))
